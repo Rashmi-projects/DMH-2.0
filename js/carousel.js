e858297319ef6039296f8d75c7a4e1930845542a
Pooja <marketing.digitalmediahub@gmail.com>
2020-07-13 19:13:54 +0530

    with counter part

--- js/carousel.js
+++ js/carousel.js
@@ -1,43 +1,46 @@
 
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
-//  var $e = $(e.relatedTarget);
-//   var idx = $e.index();
-//   var itemsPerSlide = 4;
-//   var totalItems = $('.carousel-item').length;
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
   
-//   if (idx <= totalItems-(itemsPerSlide-1)) {
-//       var it = itemsPerSlide - (totalItems - idx);
-//       for (var i=0; i<it; i++) {
-//           // append slides to end
-//           if (e.direction=="left") {
-//               $('.carousel-item').eq(i).appendTo('.carousel-inner');
-//           }
-//           else {
-//               $('.carousel-item').eq(0).appendTo('.carousel-inner');
-//           }
-//       }
-//   }
-// });
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
 
 
-// $('#carouselExample').carousel({ 
-//               interval: 2000
-//       });
+  $('#carouselExample').carousel({ 
+                interval: 2000
+        });
 
 
-// $(document).ready(function() {
-// /* show lightbox when clicking a thumbnail */
-//   $('a.thumb').click(function(event){
-//     event.preventDefault();
-//     var content = $('.modal-body');
-//     content.empty();
-//       var title = $(this).attr("title");
-//       $('.modal-title').html(title);        
-//       content.html($(this).html());
-//       $(".modal-profile").modal({show:true});
-//   });
+  $(document).ready(function() {
+/* show lightbox when clicking a thumbnail */
+    $('a.thumb').click(function(event){
+      event.preventDefault();
+      var content = $('.modal-body');
+      content.empty();
+        var title = $(this).attr("title");
+        $('.modal-title').html(title);        
+        content.html($(this).html());
+        $(".modal-profile").modal({show:true});
+    });
 
-// });
+  });
 
 // $('.counter-text').each(function() {
 //   var $this = $(this),
@@ -61,5 +64,7 @@
 //   });  
   
 // });
- 
- 
+
+
+
+
