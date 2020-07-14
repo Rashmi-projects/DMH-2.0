99670acdf5729613d93dc94654d6607513a4bb9c
Pooja <marketing.digitalmediahub@gmail.com>
2020-07-14 13:05:29 +0530

    New Changes

--- js/carousel.js
+++ js/carousel.js
@@ -1,70 +1,33 @@
-
+   
 
 $('#carouselExample').on('slide.bs.carousel', function (e) {
 
   
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 4;
-    var totalItems = $('.carousel-item').length;
-    
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
-    }
-});
-
-
-  $('#carouselExample').carousel({ 
-                interval: 2000
-        });
-
-
-  $(document).ready(function() {
-/* show lightbox when clicking a thumbnail */
-    $('a.thumb').click(function(event){
-      event.preventDefault();
-      var content = $('.modal-body');
-      content.empty();
-        var title = $(this).attr("title");
-        $('.modal-title').html(title);        
-        content.html($(this).html());
-        $(".modal-profile").modal({show:true});
-    });
-
+  var $e = $(e.relatedTarget);
+  var idx = $e.index();
+  var itemsPerSlide = 4;
+  var totalItems = $('.carousel-item').length;
+  
+  if (idx >= totalItems-(itemsPerSlide-1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i=0; i<it; i++) {
+          // append slides to end
+          if (e.direction=="left") {
+              $('.carousel-item').eq(i).appendTo('.carousel-inner');
+          }
+          else {
+              $('.carousel-item').eq(0).appendTo('.carousel-inner');
+          }
+      }
+  }
   });
-
-// $('.counter-text').each(function() {
-//   var $this = $(this),
-//       countTo = $this.attr('data-count');
   
-//   $({ countNum: $this.text()}).animate({
-//     countNum: countTo
-//   },
-
-//   {
-//    duration: 8000,
-//     easing:'linear',
-//     step: function() {
-//       $this.text(Math.floor(this.countNum));
-//     },
-//     complete: function() {
-//       $this.text(this.countNum);
-//       //alert('finished');
-//     }
-
-//   });  
   
-// });
-
+  $('#carouselExample').carousel({ 
+              interval: 2000
+      });
+  
+  });
 
 
 
