0ec8a590ca46e8827743ff7fa1b11aead5c0e68c
Pooja <marketing.digitalmediahub@gmail.com>
2020-08-24 15:51:55 +0530

    some files changed

--- mail.php
+++ mail.php
@@ -7,10 +7,10 @@ if(isset( $_POST['cname']))
 $cname = $_POST['cname'];
 if(isset( $_POST['numb']))
 $numb = $_POST['numb'];
-$subject = "A User Filled DOWNLOAD AGENCY Form";
+$subject = "A user filled DOWNLOAD AGENCY Form";
 
 $content="From: $name \n Email: $email \n Company-Name: $cname";
-$recipient = "pooja.dnagar22@gmail.com";
+$recipient = "marketing.digitalmediahub@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $cname, $content, $mailheader) or die("Error!");
 echo "Email sent!";
